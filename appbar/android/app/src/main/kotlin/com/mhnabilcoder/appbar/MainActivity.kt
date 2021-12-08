b7a7d159cadcc4b8acf7ce7867d88279fe0dd029
Mahedi Hasan Nabil <85394911+mhnabilcoder@users.noreply.github.com>
2021-12-09 00:27:42 +0600

    new commit
    formated previous files

--- appbar/android/app/src/main/kotlin/com/mhnabilcoder/appbar/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mhnabilcoder.appbar
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- class_11_prac/android/app/src/main/kotlin/com/example/class_11_prac/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.class_11_prac
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
